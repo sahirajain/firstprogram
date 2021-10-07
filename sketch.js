eb0c6dc8dff15198b780719e8c1354abbb52ab64
sahirajain <92089689+sahirajain@users.noreply.github.com>
2021-10-07 15:58:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var box
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(200,200,100,100);
+}
+
+function draw() 
+{
+  
+  background(30);
+  if (keyIsDown(UP_ARROW)) {
+   box.position.y=box.position.y-5
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    box.position.y=box.position.y+5
+   }
+  drawSprites()
+
+}
+
+
+
+
